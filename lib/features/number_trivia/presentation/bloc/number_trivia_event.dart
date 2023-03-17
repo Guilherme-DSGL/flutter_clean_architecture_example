7ac04aa6cdfe31605559f33522822ce0bdee5c10
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2023-03-17 12:30:31 -0300

    bloc

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaEvent extends Equatable {
+  const NumberTriviaEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final int number;
+  const GetTriviaForConcreteNumber({
+    required this.number,
+  });
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {
+  final int number;
+  const GetTriviaForRandomNumber({
+    required this.number,
+  });
+}
